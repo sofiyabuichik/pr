35edc4605b14ec2e653e10e0e16b107145f28248
baroman4ik <baroman4i@mail.ru>
2022-12-12 12:43:20 +0300

    add fix styles

--- src/Components/HomeTextBlock/HomeTextBlock.jsx
+++ src/Components/HomeTextBlock/HomeTextBlock.jsx
@@ -8,11 +8,11 @@ export default function HomeTextBlock() {
         <h3>Не знаешь какую карту тебе лучше выбрать? Заходи и смотри, что для тебя будет Выгодно!</h3>
         <Button variant={"outlined"} id='learn-more'>Узнай больше</Button>
         <div className='img-phone'>
-            <img className='img-phone-1' src='phone'></img>
-            <img className='img-dialog-1' src='dialog-1'></img>
-            <img className='img-dialog-2' src='dialog-2'></img>
-            <img className='img-pic' src='pic'></img>
-            <img className='img-circle-bg' src='circles-bg'></img>
+            <img className='img-phone-1' src='phone.png'></img>
+            <img className='img-dialog-1' src='dialog-1.png'></img>
+            <img className='img-dialog-2' src='dialog-2.png'></img>
+            <img className='img-pic' src='pic.png'></img>
+            <img className='img-circle-bg' src='circles-bg.png'></img>
         </div>
     </div>
   )
